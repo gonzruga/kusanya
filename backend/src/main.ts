ad11b919a29e54f08e49c0ad8ed38bbd0b75f4d2
Gonzaga Rugambwa <grugambwa@gmail.com>
2026-08-30 19:41:01 +0300

    Backend main

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,22 +6,30 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3001',
+    origin: process.env.CORS_ORIGIN ?? 'http://localhost:3001',
   });
 
   app.useGlobalPipes(
-  new ValidationPipe({
-    whitelist: true,
-    transform: true,
-    forbidNonWhitelisted: true,
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
   }),
 );
 
+  const port = Number(process.env.PORT ?? 3000);
+
   console.log(`KUSANYA API running BEFORE listening on port ${process.env.PORT ?? 3000}`);
 
-  await app.listen(process.env.PORT ?? 3000);
+  // await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 
   console.log(`KUSANYA API running on port ${process.env.PORT ?? 3000}`);
+  console.log('2 KUSANYA API running on port ${port}');
 }
 bootstrap();
 
+  // app.enableCors({
+  //   // origin: 'http://localhost:3001',
+  //   origin: process.env.CORS_ORIGIN ?? 'http://localhost:3001',
+  // });
